d1db339408830cf9f3367a4a082b4b3226abd1a5
Thiago Ozawa <72313676+YoruAlptraum@users.noreply.github.com>
2025-09-18 09:51:47 -0300

    refactor button code

--- extension/src/components/Button/Button.jsx
+++ extension/src/components/Button/Button.jsx
@@ -1,26 +1,15 @@
 const Button = ({ id, text, loading = false, onClick = () => {} }) => {
   return (
     <button id={id} className="button" disabled={loading} onClick={onClick}>
-      {loading ? (
-        <span className="loading">
-          <div className="flex">
-            <div className="loader"></div>
-            <div className="loader"></div>
-            <div className="loader"></div>
-          </div>
-          <p>{text}</p>
-        </span>
-      ) : (
-        <span className="not-loading">
-          <div className="flex h-6 overflow-visible">
-            <div className="loader"></div>
-            <div className="loader"></div>
-            <div className="loader"></div>
-          </div>
-          <p>{text}</p>
-          <p>{text}</p>
-        </span>
-      )}
+      <span className={`${loading ? "loading" : "not-loading"}`}>
+        <div className="flex h-6 w-24 overflow-visible mx-auto">
+          <div className="loader"></div>
+          <div className="loader"></div>
+          <div className="loader"></div>
+        </div>
+        <p className="justify-center inline-flex h-6">{text}</p>
+        <p>{text}</p>
+      </span>
     </button>
   );
 };
